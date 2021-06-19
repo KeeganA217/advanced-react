588c7dc7972eb950faa45b714014033cf1ba23d1
Keegan <keegan.adams15@gmail.com>
2021-06-19 10:21:18 -0500

    new unit tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -8,7 +8,21 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
 
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it("has a textarea and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
+});
+
+it("has a textarea that users can type in", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 });
