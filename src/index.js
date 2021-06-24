6e4eebbe20edac8744cb473e23557297ebae918e
Keegan <keegan.adams15@gmail.com>
2021-06-24 16:50:14 -0500

    added router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
+import { BrowserRouter, Route } from "react-router-dom";
 import Root from "./Root";
+
 ReactDom.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>,
   document.querySelector("#root")
 );
